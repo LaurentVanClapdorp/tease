01368c206e478db30a652f7cf4185c24a3b9175e
Laurent <loron@live.be>
2021-01-08 11:17:39 +0100

    Gatsby folder structuring

---
+++ src/templates/templateStyles/constants.js
@@ -0,0 +1,21 @@
+export const COLORS = {
+  PRIMARY: "#014750",
+  SECONDARY: "#3BBFCF",
+  TERTIARY: "#B993C6",
+  QUADRI: "#633473",
+  WHITE: "#EEFAFC",
+  BLACK: "#021E21",
+  GREY: "#485D60",
+}
+
+export const FONT_FAMILIES = {
+  TITLE: "Oswald",
+  TEXT: "Roboto",
+  BUTTON: "Oswald",
+}
+
+export const MEDIA_QUERIES = {
+  SMALL: "520px",
+  MEDIUM: "980px",
+  LARGE: "1200px",
+}
